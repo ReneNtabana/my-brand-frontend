321644398f7f73476a8576337e5859fd1b381093
Reennee <r.ntabana@alustudent.com>
2023-03-10 01:10:01 +0200

    integrating signup page

--- Signup.js
+++ Signup.js
@@ -5,7 +5,52 @@ const password = document.getElementById('user-password');
 const password2 = document.getElementById('user-password2');
 const signup = document.getElementById('sign-up-btn');
 
-
+form.addEventListener('submit', async (event) => {
+    const username = document.querySelector('#username').value;
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+    //   const password2 = document.querySelector('#password2').value;
+    event.preventDefault();
+  
+    fetch('https://repulsive-frog-jacket.cyclic.app/api/register', {
+      // fetch('http://127.0.0.1:5000/api/register', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({ username, email, password }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((resp) => {
+        console.log(resp);
+        if (resp.data) {
+          // location.href =
+        } else {
+          alert(resp.message);
+        }
+        return resp;
+      });
+  });
+  
+  //Show Password Function
+  function showPassword() {
+    var show = document.getElementById('password');
+    var show2 = document.getElementById('password2');
+  
+    if (show.type == 'password') {
+      show.type = 'text';
+    } else {
+      show.type = 'password';
+    }
+  
+    if (show2.type == 'password') {
+      show2.type = 'text';
+    } else {
+      show2.type = 'password';
+    }
+  }
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -75,7 +120,7 @@ const validateInputs = () => {
     }
 
  // If the form is valid, store the user's information in local storage
-      const user = { usernameValue,emailValue, passwordValue };
+      const user = { username,email, password };
       localStorage.setItem("user", JSON.stringify(user));
   
       // Redirect to the dashboard
